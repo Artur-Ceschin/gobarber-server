826ef5a11c4b40ab7c897210511373dd1ffca569
Artur Ceschin <artur.ceschin@gmail.com>
2023-09-12 11:15:39 -0300

    feat: 🎸 intial project and basic architecture

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
-
-app.post("/", (request, response) => {});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log("Application running on port 3333");
